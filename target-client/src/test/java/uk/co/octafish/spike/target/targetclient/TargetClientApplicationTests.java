83e938e9e46d22827583e07bbbd9e06b523d0a3e
octafish <88k-nNs-EZE-M92>
2017-09-27 12:07:11 +0100

    Tests

---
+++ target-client/src/test/java/uk/co/octafish/spike/target/targetclient/TargetClientApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.co.octafish.spike.target.targetclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TargetClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
